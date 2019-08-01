d0da36347e186d0d34360f7c808180fcdfcbefe3
Wu, Rishen <rishen.wu@zeiss.com>
2019-08-01 17:32:05 +0800

    add docs

---
+++ docs/precache-manifest.6f41b0ae1480206450ff8074666be3e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "136251b7af19847ed78c914a59834ee6",
+    "url": "/rishenwu/ReactApp/index.html"
+  },
+  {
+    "revision": "f1b843b2627c009d86d2",
+    "url": "/rishenwu/ReactApp/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/rishenwu/ReactApp/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "f1b843b2627c009d86d2",
+    "url": "/rishenwu/ReactApp/static/js/main.b565c31b.chunk.js"
+  },
+  {
+    "revision": "8904049d04bb9edbc976",
+    "url": "/rishenwu/ReactApp/static/js/runtime~main.e1b6c73f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/rishenwu/ReactApp/static/media/logo.5d5d9eef.svg"
+  }
+]);
